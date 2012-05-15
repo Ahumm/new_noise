0b730cd3f9ac82f09f4a79289e0388bca9b04d53
Tate Larsen <larset2@gmail.com>
2012-05-14 22:55:28 -0400

    first additions

---
+++ modules/noise_includes.h
@@ -0,0 +1,11 @@
+#ifndef _NOISE_INCLUDES_H_
+#define _NOISE_INCLUDES_H_
+
+#include "noise_types.h"
+#include "noise_math.h"
+#include "noise_constants.h"
+
+#include "base_mod.h"
+
+#endif
+
---
+++ modules/noise_type.h
@@ -0,0 +1,11 @@
+#ifndef _NOISE_TYPES_H_
+#define _NOISE_TYPES_H_
+
+#include <stdint.h>
+
+namespace noise
+{
+    typedef uint32_t n_uint;
+}
+
+#endif
